e4a4e68024634225720c65612b43f64dc269b9f6
Marcus Nilsson <tandy.nilsson@gmail.com>
2012-08-24 19:34:11 +0200

    test

---
+++ server/src/tracktrain_db.erl
@@ -0,0 +1,24 @@
+%% Author: marcus
+%% Created: 24 aug 2012
+%% Description: TODO: Add description to db
+-module(tracktrain_db).
+
+%%
+%% Include files
+%%
+
+%%
+%% Exported Functions
+%%
+-export([]).
+
+%%
+%% API Functions
+%%
+
+
+
+%%
+%% Local Functions
+%%
+
